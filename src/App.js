b360a52f4d15b6419c125a954c2b56a991a3d572
Ajith-Bondili <ajithbon05@gmail.com>
2024-10-16 15:00:20 -0400

    switched back to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainMenu from './components/MainMenu';
 import GameModeMenuSinglePlayer from './components/GameModeMenuSinglePlayer';
 import GameModeMenuTwoPlayer from './components/GameModeMenuTwoPlayer';
